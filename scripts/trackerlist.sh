03daf496792627653b02cb8da7d97299199fbb88
Milan Hauth <milahu@gmail.com>
2024-10-31 15:17:18 +0100

    up scripts/trackerlist.sh

--- scripts/trackerlist.sh
+++ scripts/trackerlist.sh
@@ -3,18 +3,45 @@
 set -e
 #set -x # debug
 
+function check_command() {
+  local cmd
+  for cmd in "$@"; do
+    if command -v "$cmd" >/dev/null; then
+      continue
+    fi
+    echo "error: missing command: $cmd"
+    exit 1
+  done
+}
+
+check_command curl netselect grep sed xargs
+
 # https://github.com/ngosang/trackerslist
-url="https://github.com/ngosang/trackerslist/raw/master/trackers_best_ip.txt"
+url="https://github.com/ngosang/trackerslist/raw/master/trackers_all_ip.txt"
 echo "fetching $url"
-if ! trackers_best_ip="$(curl -sL "$url")"; then
-  echo "error: failed to fetch trackers_best_ip"
+if ! trackers_all_ip="$(curl -sL "$url")"; then
+  echo "error: failed to fetch trackers_all_ip"
   exit 1
 fi
-# remove empty lines
-trackers_best_ip="$(echo "$trackers_best_ip" | grep .)"
+
+# select nearest trackers for this location
+# this requires root access for ping
+# sed -E 's/^\s*([0-9]+) (.*)$/\2#dt=\1ms/'
+num_pings=100
+num_servers=5
+netselect_result="$(
+  echo "$trackers_all_ip" |
+  grep '^udp://' |
+  xargs sudo netselect -v -s999 -t$num_pings
+)"
+
+echo "writing trackerlist.netselect.txt"
+echo "$netselect_result" >trackerlist.netselect.txt
 
 trackerlist="$(
-  echo "$trackers_best_ip"
+  echo "$netselect_result" |
+  head -n$num_servers |
+  sed -E 's/^\s*([0-9]+) (.*)$/\2/'
 
   # https://rutracker.org/
   echo "http://bt.t-ru.org/ann?magnet"
