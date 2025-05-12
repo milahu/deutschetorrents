05de98389fed7166196c0b3f0d466a532827c70b
Milan Hauth <milahu@gmail.com>
2025-05-12 19:31:48 +0200

    add src/dht/multi-ger.txt

--- src/magnet-files.txt.sh
+++ src/magnet-files.txt.sh
@@ -6,7 +6,10 @@ echo "writing src/magnet-files.txt"
 
 ls \
   src/dht/german-* \
+  src/dht/multi-ger.txt \
   src/dht/todo.txt \
   src/rutracker.org/todo.txt \
   src/rutracker.org/magnets.txt \
+  src/trash/magnets.txt \
+  src/temp-seeds/magnets.txt \
   | sort >src/magnet-files.txt
