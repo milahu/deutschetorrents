835b80effb9a2cc9a3c2fccc4192209730b50f26
Milan Hauth <milahu@gmail.com>
2025-08-18 16:30:45 +0200

    fix grep.sh

--- scripts/grep.sh
+++ scripts/grep.sh
@@ -2,6 +2,13 @@
 
 #set -eux
 
-regex="$1"
+regex=
+for arg in "$@"; do
+  regex+="$arg."
+done
+regex="${regex:0: -1}"
+regex="magnet:\S+dn=\S*$regex\S+"
 
-grep -i -E -h -o "magnet:\S+dn=\S*$regex\S+" magnets.txt tempseeds.txt $(cat src/magnet-files.txt) issues/github/md/*
+# echo "regex: $regex" >&2 # debug
+
+grep -i -E -h -o "$regex" magnets.txt tempseeds.txt $(cat src/magnet-files.txt) issues/github/md/*
